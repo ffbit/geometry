45df2ef91a531240acdf27818c852d6bb85341a9
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2015-12-06 21:26:44 +0100

    Implemented LineSegment.

---
+++ src/main/java/com/ffbit/geometry/LineSegment.java
@@ -0,0 +1,71 @@
+package com.ffbit.geometry;
+
+import java.util.Objects;
+
+/**
+ * <p>
+ * A directed line segment from point a to point b.
+ * </p>
+ *
+ * @see <a href="https://en.wikipedia.org/wiki/Line_segment">Line segment</a>
+ * @see <a href="https://en.wikipedia.org/wiki/Line_(geometry)">Line</a>.
+ */
+public class LineSegment {
+    private final Point a;
+    private final Point b;
+
+    public LineSegment(Point a, Point b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public Point getA() {
+        return a;
+    }
+
+    public Point getB() {
+        return b;
+    }
+
+    public int length() {
+        int dx = a.getX() - b.getX();
+        int dy = a.getY() - b.getY();
+
+        return (int) Math.sqrt(dx * dx + dy * dy);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + a.getX() + " " + a.getY()
+                + ", " + b.getX() + " " + b.getY() + ")";
+    }
+
+    public boolean pointIsToTheRightSide(Point point) {
+        return twiceTheSignedArea(point) < 0;
+    }
+
+    private int twiceTheSignedArea(Point point) {
+        return (b.getX() - a.getX()) * (point.getY() - a.getY())
+                - (b.getY() - a.getY()) * (point.getX() - a.getX());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+
+        LineSegment other = (LineSegment) o;
+        return Objects.equals(a, other.a)
+                && Objects.equals(b, other.b);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(a, b);
+    }
+
+}
---
+++ src/test/java/com/ffbit/geometry/LineSegmentTest.java
@@ -0,0 +1,148 @@
+package com.ffbit.geometry;
+
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.hasToString;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
+
+@RunWith(JUnitParamsRunner.class)
+public class LineSegmentTest {
+    private Point anyPoint = new Point(-1, -1);
+
+    @Test
+    public void itShouldHavePointA() {
+        Point a = new Point(1, 2);
+
+        LineSegment lineSegment = new LineSegment(a, anyPoint);
+
+        assertThat(lineSegment.getA(), is(a));
+    }
+
+    @Test
+    public void itShouldHavePointB() {
+        Point b = new Point(3, 4);
+
+        LineSegment lineSegment = new LineSegment(anyPoint, b);
+
+        assertThat(lineSegment.getB(), is(b));
+    }
+
+    @Test
+    @Parameters(method = "lineLengths")
+    public void itShouldHaveLength(LineSegment lineSegment, int length) {
+        assertThat(lineSegment.length(), is(length));
+    }
+
+    private Object[][] lineLengths() {
+        return new Object[][]{
+                {new LineSegment(new Point(0, 0), new Point(3, 4)), 5},
+                {new LineSegment(new Point(53, 40), new Point(59, 48)), 10}
+        };
+    }
+
+    @Test
+    public void itShouldHaveToString() {
+        Point a = new Point(0, 1);
+        Point b = new Point(2, 3);
+
+        LineSegment lineSegment = new LineSegment(a, b);
+
+        assertThat(lineSegment, hasToString("(0 1, 2 3)"));
+    }
+
+    @Test
+    @Parameters(method = "rightSidePoints")
+    public void itShouldFindIfThirdPointIsToTheRightSide(LineSegment segment,
+                                                         Point point) {
+        assertThat(segment.pointIsToTheRightSide(point), is(true));
+    }
+
+    private Object[][] rightSidePoints() {
+        return new Object[][]{
+                {new LineSegment(new Point(1, 2), new Point(5, 6)),
+                        new Point(4, 3)},
+                {new LineSegment(new Point(0, 0), new Point(2, 0)),
+                        new Point(-1, -1)},
+                {new LineSegment(new Point(2, 2), new Point(2, -1)),
+                        new Point(1, 2)}
+        };
+    }
+
+    @Test
+    @Parameters(method = "leftSidePoints,collinearPoints")
+    public void itShouldFindIfThirdPointIsNotToTheRightSide(LineSegment segment,
+                                                            Point point) {
+        assertThat(segment.pointIsToTheRightSide(point), is(false));
+    }
+
+    private Object[][] leftSidePoints() {
+        return new Object[][]{
+                {new LineSegment(new Point(1, 2), new Point(5, 6)),
+                        new Point(3, 5)},
+                {new LineSegment(new Point(0, 0), new Point(2, 0)),
+                        new Point(1, 1)},
+                {new LineSegment(new Point(2, 2), new Point(2, -1)),
+                        new Point(3, 1)}
+        };
+    }
+
+    private Object[][] collinearPoints() {
+        return new Object[][]{
+                {new LineSegment(new Point(1, 2), new Point(5, 6)),
+                        new Point(3, 4)},
+                {new LineSegment(new Point(0, 0), new Point(2, 0)),
+                        new Point(-1, 0)},
+                {new LineSegment(new Point(2, 2), new Point(2, -1)),
+                        new Point(2, 3)}
+        };
+    }
+
+    @Test
+    @Parameters(method = "equalSegmentsOfSameDirection")
+    public void itShouldBeEqualToAnotherSegmentOfSameDirection(LineSegment ab,
+                                                               LineSegment cd) {
+        assertThat(ab, is(cd));
+    }
+
+    @Test
+    @Parameters(method = "equalSegmentsOfSameDirection")
+    public void twoEqualSegmentsShouldHaveTheSameHashCode(LineSegment ab,
+                                                          LineSegment cd) {
+        assertThat(ab.hashCode(), is(cd.hashCode()));
+    }
+
+    private Object[][] equalSegmentsOfSameDirection() {
+        LineSegment sameSegment =
+                new LineSegment(new Point(0, 0), new Point(1, 1));
+
+        return new Object[][]{
+                {sameSegment, sameSegment},
+                {new LineSegment(new Point(0, 1), new Point(2, 3)),
+                        new LineSegment(new Point(0, 1), new Point(2, 3))}
+        };
+    }
+
+    @Test
+    @Parameters(method = "unequalSegments")
+    public void itShouldBeEqualToAnotherSegment(LineSegment ab,
+                                                LineSegment cd) {
+        assertThat(ab, is(not(cd)));
+    }
+
+    private Object[][] unequalSegments() {
+        return new Object[][]{
+                {new LineSegment(new Point(0, 1), new Point(2, 3)), null},
+                {new LineSegment(new Point(0, 1), new Point(2, 3)),
+                        new LineSegment(new Point(4, 5), new Point(6, 7))},
+
+                {new LineSegment(new Point(0, 1), new Point(2, 3)),
+                        new LineSegment(new Point(2, 3), new Point(0, 1))}
+        };
+    }
+
+}
