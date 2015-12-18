1a39e654b43d7c402057976b6a78d05213922dd0
Dmytro Chyzhykov <dmytro.chyzhykov@yandex.ru>
2015-12-18 22:54:36 +0100

    A O(h N) Jarvis's march Gift Wrapping convex hull algorithm implementation.

--- src/main/java/com/ffbit/geometry/convexhull/GiftWrappingConvexHull.java
+++ src/main/java/com/ffbit/geometry/convexhull/GiftWrappingConvexHull.java
@@ -4,64 +4,57 @@ import com.ffbit.geometry.LineSegment;
 import com.ffbit.geometry.Point;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
  * <p>
- * A O(h N<sup>2</sup>) Gift Wrapping convex hull algorithm.
+ * A O(h N) Jarvis's march Gift Wrapping convex hull algorithm implementation.
+ *
+ * @see <a href="https://en.wikipedia.org/wiki/Gift_wrapping_algorithm">Gift wrapping algorithm</a>
  * </p>
  */
 public class GiftWrappingConvexHull implements ConvexHull {
 
     @Override
     public List<Point> convexHull(List<Point> points) {
-        List<Point> sortedPoints = sortPoints(points);
+        Point lowestLeftmostPoint = findLowestLeftmostPoint(points);
 
         List<Point> convexHull = new ArrayList<>();
-        Point startPoint = sortedPoints.get(0);
-        Point convexPoint = startPoint;
+        Point pointOnHull = lowestLeftmostPoint;
 
         do {
-            convexHull.add(convexPoint);
-            convexPoint = findNextConvexPoint(sortedPoints, convexPoint);
-        } while (!startPoint.equals(convexPoint));
+            convexHull.add(pointOnHull);
+
+            Point endPoint = points.get(0);
+            for (Point point : points) {
+                LineSegment segment = new LineSegment(pointOnHull, point);
+
+                if (segment.pointIsToTheRightSide(endPoint)
+                        || segment.pointLiesOnSegment(endPoint)) {
+                    endPoint = point;
+                }
+            }
+
+            pointOnHull = endPoint;
+        } while (!pointOnHull.equals(lowestLeftmostPoint));
 
         return convexHull;
     }
 
-    private Point findNextConvexPoint(List<Point> points, Point convexPoint) {
-        Point nextConvexPoint = convexPoint;
+    private Point findLowestLeftmostPoint(List<Point> points) {
+        Point lowestLeftmostPoint = points.get(0);
 
-        for (Point candidateConvexPoint : points) {
-            LineSegment segment =
-                    new LineSegment(convexPoint, candidateConvexPoint);
-            boolean valid = true;
-
-            for (int i = 0; i < points.size(); i++) {
-                Point point = points.get(i);
-
-                if (!(segment.pointIsToTheRightSide(point)
-                        || segment.pointLiesOnSegment(point))) {
-                    valid = false;
-                    break;
-                }
-            }
-
-            if (valid) {
-                nextConvexPoint = candidateConvexPoint;
-                break;
+        for (Point point : points) {
+            if (less(point, lowestLeftmostPoint)) {
+                lowestLeftmostPoint = point;
             }
         }
 
-        return nextConvexPoint;
+        return lowestLeftmostPoint;
     }
 
-    private List<Point> sortPoints(List<Point> points) {
-        List<Point> sortedPoints = new ArrayList<>(points);
-        Collections.sort(sortedPoints);
-
-        return sortedPoints;
+    private boolean less(Point a, Point b) {
+        return a.compareTo(b) < 0;
     }
 
 }
